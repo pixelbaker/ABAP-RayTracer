d790b1ad64d2cb0b354f39bd1f4ed813b615d894
Pixelbaker <mail@pixelbaker.com>
2017-10-09 22:46:13 +0200

    Create importing_reference.cpp

---
+++ code_examples/method_signatures/importing_reference.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+class Foo {
+public:
+    void foobar(const int& value);
+};
+
+void Foo::foobar(const int& i_value) {
+    std::cout << i_value << "\n";
+}
+
+int main(int argc, const char * argv[]) {
+    Foo instance = Foo();
+    int value = 42;
+    instance.foobar( value );
+    return 0;
+}
