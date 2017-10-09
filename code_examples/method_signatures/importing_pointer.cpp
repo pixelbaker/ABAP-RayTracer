5c64af0a015318f2a231b910db40e48dfe6c307a
Pixelbaker <mail@pixelbaker.com>
2017-10-09 22:49:30 +0200

    Create importing_pointer.cpp

---
+++ code_examples/method_signatures/importing_pointer.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+class Foo {
+public:
+    void foobar(const int* value);
+};
+
+void Foo::foobar(const int* i_value) {
+    std::cout << *i_value << "\n";
+}
+
+int main(int argc, const char * argv[]) {
+    Foo instance = Foo();
+    int value = 42;
+    int* value_ptr = &value;
+    instance.foobar( value_ptr );
+    return 0;
+}
