9c02c16ce0532b8cc2286867ad70d4ec99f2233b
Pixelbaker <mail@pixelbaker.com>
2017-10-09 22:29:33 +0200

    Create returning_pointer.cpp

---
+++ code_examples/method_signatures/returning_pointer.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+class Foo {
+public:
+    int* foobar();
+};
+
+int* Foo::foobar() {
+    return (new int(42));
+}
+
+int main(int argc, const char * argv[]) {
+    Foo instance = Foo();
+    int* value = instance.foobar( );
+    std::cout << *value << "\n";
+    return 0;
+}
